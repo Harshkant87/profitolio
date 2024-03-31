7dba2a35b1f737aa32af2dbdb45c115c376452e7
Harshkant87 <harshkant87@gmail.com>
2024-03-31 16:46:57 +0530

    Sign Up Page

--- crm/profitolio/views.py
+++ crm/profitolio/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .forms import CreateUserForm, LoginForm
 
 # Create your views here.
 
+# HomePage
 def home(request):
     return render(request, 'profitolio/index.html')
+
+
+# Reister
+def register(request):
+
+    form = CreateUserForm()
+
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # return redirect('')
+
+    context = {'form': form}
+    return render(request, 'profitolio/register.html', context = context)
