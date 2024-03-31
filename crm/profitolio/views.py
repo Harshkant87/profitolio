5d85c5c484efdce5d9302cc2d8a67ac28a73b84f
Harshkant87 <harshkant87@gmail.com>
2024-03-31 18:40:53 +0530

    Login page

--- crm/profitolio/views.py
+++ crm/profitolio/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 from .forms import CreateUserForm, LoginForm
+from django.contrib.auth.models import auth
+from django.contrib.auth import authenticate
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -17,7 +20,38 @@ def register(request):
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
-            # return redirect('')
+            return redirect('my-login')
 
     context = {'form': form}
     return render(request, 'profitolio/register.html', context = context)
+
+#Login a user
+def my_login(request):
+    form = LoginForm()
+
+    if request.method == "POST":
+        form = LoginForm(request, data = request.POST)
+        if form.is_valid():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username = username, password = password)
+
+            if user is not None:
+                auth.login(request, user)
+                return redirect('dashboard')
+    
+    context = {'form2': form}
+    return render(request, 'profitolio/my-login.html', context = context)
+
+#dashboard
+@login_required(login_url='my-login')
+def dashboard(request):
+    return render(request, 'profitolio/dashboard.html')
+
+
+#Logout
+def user_logout(request):
+    auth.logout(request)
+    return redirect("my-login")
+
